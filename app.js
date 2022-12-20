d84f46ce1288b05722555f6fe1984a329bb28b1f
Joshua <j2h5068312@gmail.com>
2022-12-20 22:31:21 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
